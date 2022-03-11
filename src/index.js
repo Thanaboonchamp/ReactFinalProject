4246151f8b88d0dafb9df021b92125db2aa04dd2
thanaboonchamp <lu.thanaboon_st@tni.ac.th>
2022-03-11 19:43:19 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import GlobalStyled from './styles/GlobalStyle';
+import { BrowserRouter } from'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+
+    <GlobalStyled />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
